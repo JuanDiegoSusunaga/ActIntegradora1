5a147df19ab7d41f21a41c13f53879e93b2911e3
JuanDiegoSusunaga <138306321+JuanDiegoSusunaga@users.noreply.github.com>
2025-05-21 21:15:13 -0600

    Create test_palindromoMasLargo.cpp

---
+++ tests/test_palindromoMasLargo.cpp
@@ -0,0 +1,15 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "doctest.h"
+#include "../src/main.cpp"
+
+TEST_CASE("palindromoMasLargo - caso normal") {
+    auto [inicio, fin, pal] = palindromoMasLargo("abcddcba");
+    CHECK(inicio == 1);
+    CHECK(fin == 8);
+    CHECK(pal == "abcddcba");
+}
+
+TEST_CASE("palindromoMasLargo - sin palíndromos largos") {
+    auto [inicio, fin, pal] = palindromoMasLargo("abc");
+    CHECK(pal == "a");
+}
